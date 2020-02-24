200ec4885cbf7d2792d94a9bc77ecae4e26473a3
AndSelm <and.selm+git@gmail.com>
2020-02-24 16:32:51 +0100

    FEAT: adding @typescript-eslint/indet

--- typescript-changes.js
+++ typescript-changes.js
@@ -1,6 +1,27 @@
 module.exports = {
   rules: {
     '@typescript-eslint/semi': ['error'],
+    '@typescript-eslint/indent': ['error', 2, {
+      SwitchCase: 1,
+      VariableDeclarator: 1,
+      outerIIFEBody: 1,
+      FunctionDeclaration: {
+        parameters: 'first',
+        body: 1
+      },
+      FunctionExpression: {
+        parameters: 'first',
+        body: 1
+      },
+      CallExpression: {
+        arguments: 'off'
+      },
+      ArrayExpression: 1,
+      ObjectExpression: 1,
+      ImportDeclaration: 1,
+      flatTernaryExpressions: false,
+      ignoredNodes: ['JSXElement', 'JSXElement *']
+    }],
     'import/extensions': ['error', 'always', {
       'js': 'never',
       'mjs': 'never',
